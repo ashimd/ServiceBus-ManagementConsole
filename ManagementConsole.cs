d09a2807280c1e436354f3a2795ea5c4a86d6f7a
Ashim <ashim.sp@gmail.com>
2017-11-15 13:02:57 +0530

    Add files via upload

---
+++ ManagementConsole.cs
@@ -0,0 +1,79 @@
+﻿using System;
+
+namespace ServiceBusManagementConsole
+{
+    class ManagementConsole
+    {
+        private static string ServiceBusConnectionString = "";
+
+        static void Main(string[] args)
+        {
+            ManagementHelper helper = new ManagementHelper(ServiceBusConnectionString);
+
+            do
+            {
+                Console.ForegroundColor = ConsoleColor.Cyan;
+                Console.Write(">");
+                string commandLine = Console.ReadLine();
+                Console.ForegroundColor = ConsoleColor.Magenta;
+                string[] commands = commandLine.Split(' ');
+
+                try
+                {
+                    if (commands.Length > 0)
+                    {
+                        switch (commands[0])
+                        {
+                            case "createqueue":
+                            case "cq":
+                                if (commands.Length > 1)
+                                {
+                                    helper.CreateQueue(commands[1]);
+                                }
+                                else
+                                {
+                                    Console.ForegroundColor = ConsoleColor.Yellow;
+                                    Console.WriteLine("Queue path not specified.");
+                                }
+                                break;
+                            case "listqueues":
+                            case "lq":
+                                helper.ListQueues();
+                                break;
+                            case "getqueue":
+                            case "gq":
+                                if (commands.Length > 1)
+                                {
+                                    helper.GetQueue(commands[1]);
+                                }
+                                else
+                                {
+                                    Console.ForegroundColor = ConsoleColor.Yellow;
+                                    Console.WriteLine("Queue path not specified.");
+                                }
+                                break;
+                            case "deletequeue":
+                            case "dq":
+                                if (commands.Length > 1)
+                                {
+                                    helper.DeleteQueue(commands[1]);
+                                }
+                                else
+                                {
+                                    Console.ForegroundColor = ConsoleColor.Yellow;
+                                    Console.WriteLine("Queue path not specified.");
+                                }
+                                break;
+                            default:
+                                break;
+                        }
+                    }
+                }
+                catch (Exception)
+                {
+                    throw;
+                }
+            } while (true);
+        }
+    }
+}
---
+++ ManagementHelper.cs
@@ -0,0 +1,123 @@
+﻿using Microsoft.ServiceBus;
+using Microsoft.ServiceBus.Messaging;
+using System;
+using System.Collections.Generic;
+
+namespace ServiceBusManagementConsole
+{
+    class ManagementHelper
+    {
+        private NamespaceManager m_NamespaceManager;
+
+        internal ManagementHelper(string connectionString)
+        {
+            // using the specified credentials
+            m_NamespaceManager = NamespaceManager.CreateFromConnectionString(connectionString);
+
+            // Output the client address
+            Console.WriteLine("Service bus address {0}", m_NamespaceManager.Address);
+        }
+
+        internal void CreateQueue(string queuePath)
+        {
+            Console.Write("Creating queue {0}...", queuePath);
+            var description = GetQueueDescription(queuePath);
+            var createdDescription = m_NamespaceManager.CreateQueue(description);
+            Console.WriteLine("Done!");
+        }
+
+        internal void DeleteQueue(string queuePath)
+        {
+            Console.Write("Deleting queue {0}...", queuePath);
+            m_NamespaceManager.DeleteQueue(queuePath);
+            Console.WriteLine("Done!");
+        }
+
+        internal void ListQueues()
+        {
+            IEnumerable<QueueDescription> queueDescriptions = m_NamespaceManager.GetQueues();
+            Console.WriteLine("Listing queues...");
+            foreach (QueueDescription queueDescription in queueDescriptions)
+            {
+                Console.WriteLine("\t{0}", queueDescription.Path);
+            }
+            Console.WriteLine("Done!");
+        }
+
+        internal void GetQueue(string queuePath)
+        {
+            QueueDescription queueDescription = m_NamespaceManager.GetQueue(queuePath);
+
+            Console.WriteLine("Queue Path:                              {0}",
+                queueDescription.Path);
+            Console.WriteLine("Queue MessageCount:                      {0}",
+                queueDescription.MessageCount);
+            Console.WriteLine("Queue SizeInBytes:                      {0}",
+                queueDescription.SizeInBytes);
+            Console.WriteLine("Queue RequiresSession:                      {0}",
+                queueDescription.RequiresSession);
+            Console.WriteLine("Queue RequiresDuplicateDetection:                      {0}",
+                queueDescription.RequiresDuplicateDetection);
+            Console.WriteLine("Queue DuplicateDetectionHistoryTimeWindow:                      {0}",
+                queueDescription.DuplicateDetectionHistoryTimeWindow);
+            Console.WriteLine("Queue LockDuration:                      {0}",
+                queueDescription.LockDuration);
+            Console.WriteLine("Queue DefaultMessageTimeToLive:                      {0}",
+                queueDescription.DefaultMessageTimeToLive);
+            Console.WriteLine("Queue EnableDeadLetteringOnMessageExpiration:                      {0}",
+                queueDescription.EnableDeadLetteringOnMessageExpiration);
+            Console.WriteLine("Queue EnableBatchedOperations:                      {0}",
+                queueDescription.EnableBatchedOperations);
+            Console.WriteLine("Queue MaxSizeInMegaBytes:                      {0}",
+                queueDescription.MaxSizeInMegabytes);
+            Console.WriteLine("Queue MaxDeliveryCount:                      {0}",
+                queueDescription.MaxDeliveryCount);
+            Console.WriteLine("Queue IsReadOnly:                      {0}",
+                queueDescription.IsReadOnly);
+        }
+
+        internal void ListTopicsAndSubscriptions()
+        {
+            IEnumerable<TopicDescription> topicDescriptions = m_NamespaceManager.GetTopics();
+            Console.WriteLine("Listing topics and subscriptions...");
+            foreach (TopicDescription topicDescription in topicDescriptions)
+            {
+                Console.WriteLine("\t{0}", topicDescription.Path);
+                IEnumerable<SubscriptionDescription> subscriptionDescriptions =
+                    m_NamespaceManager.GetSubscriptions(topicDescription.Path);
+                foreach (SubscriptionDescription subscriptionDescription in subscriptionDescriptions)
+                {
+                    Console.WriteLine("\t\t{0}", subscriptionDescription.Name);
+                }
+            }
+            Console.WriteLine("Done!");
+        }
+
+        internal void CreateTopic(string topicPath)
+        {
+            Console.WriteLine("Creating topic {0}...", topicPath);
+            var description = m_NamespaceManager.CreateTopic(topicPath);
+            Console.WriteLine("Done!");
+        }
+
+        internal void CreateSubscription(string topicPath, string subscriptionName)
+        {
+            Console.WriteLine("Creating subscription {0}/subscriptions/{1}...", topicPath, subscriptionName);
+            var description = m_NamespaceManager.CreateSubscription(topicPath, subscriptionName);
+            Console.WriteLine("Done!");
+        }
+
+        private QueueDescription GetQueueDescription(string path)
+        {
+            return new QueueDescription(path)
+            {
+                RequiresDuplicateDetection = true,
+                DuplicateDetectionHistoryTimeWindow = TimeSpan.FromMinutes(10),
+                RequiresSession = true,
+                MaxDeliveryCount = 20,
+                DefaultMessageTimeToLive = TimeSpan.FromHours(1),
+                EnableDeadLetteringOnMessageExpiration = true
+            };
+        }
+    }
+}
